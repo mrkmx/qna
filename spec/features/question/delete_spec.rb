71bcd79038a92fda610a684341dfcb30c98901b7
mrkmx <alex.a.maximow@gmail.com>
2021-07-24 12:39:00 +0400

    user can del files from questions and answers

--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -13,6 +13,24 @@ feature 'User can delete question' do
       expect(page).to_not have_content 'Question String'
     end
 
+    scenario 'delete question with attached files', js: true do
+      sign_in(question.user)
+      visit question_path(question)
+      within '.question' do
+        click_on 'Edit question'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+      end
+  
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+      within "#file_#{question.files.first.id}" do
+        click_on 'Delete file'
+      end
+  
+      expect(page).to_not have_link 'rails_helper.rb'
+    end
+
     scenario "someone else's question" do
       user = create(:user)
 
