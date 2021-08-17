0684ff7bf6bd120bb4b72690beffecf7de7c65ff
mrkmx <alex.a.maximow@gmail.com>
2021-08-17 17:12:58 +0400

    link format validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   
   validates :name, :url, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https ftp]), message: "It's not a valid URL" }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  it { is_expected.to allow_values('http://example.com', 'https://example.com', 'ftp://example.com').for(:url) }
+  it { is_expected.to_not allow_values('example.com', 'example').for(:url) }
+
 end
