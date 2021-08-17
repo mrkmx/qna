136060c0594603ecea4efd81decf054aaccc8b78
mrkmx <alex.a.maximow@gmail.com>
2021-08-17 17:28:31 +0400

    check github gists and render it's

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,9 @@ class Link < ApplicationRecord
   
   validates :name, :url, presence: true
   validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https ftp]), message: "It's not a valid URL" }
+
+  def github_gist?
+    url.include?('https://gist.github.com')
+  end
+
 end
