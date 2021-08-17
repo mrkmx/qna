d0db14f239fe9861b6909821b56ec57136493211
mrkmx <alex.a.maximow@gmail.com>
2021-08-17 23:05:55 +0400

    fix test "the user can receive the award and view it"

--- spec/features/answer/best_spec.rb
+++ spec/features/answer/best_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 feature 'User can choose the best answer of his question' do
-  given(:reward) { create(:reward, question: question) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question) }
   given!(:best_answer) { create(:answer, question: question, body: 'Best', best: true) }
   given(:user) { create(:user) }
+  
+  given!(:question_owner) { create(:question, user: user) }
+  given!(:answer_owner) { create(:answer, question: question_owner, user: user) }
+  given!(:reward) { create(:reward, question: question_owner) }
 
   describe 'Authenticated user tries to choose' do
     scenario 'The best answer of his question', js: true do
@@ -46,8 +49,8 @@ feature 'User can choose the best answer of his question' do
     end
 
     scenario 'tries to see the list of the earned rewards', js: true do
-      sign_in(question.user)
-      visit question_path(question)
+      sign_in(question_owner.user)
+      visit question_path(question_owner)
   
       # within "#answer-#{answer.id}" do
       click_on 'Best answer'
