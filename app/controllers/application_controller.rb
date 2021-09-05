8f2e230f82022aebbef5a3fbaa402acbf0721f05
mrkmx <alex.a.maximow@gmail.com>
2021-09-06 01:38:53 +0400

    setup cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :fetch_shared_params
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
+  check_authorization unless: :devise_controller?
+
   private
   
   def fetch_shared_params
---
+++ app/models/ability.rb
@@ -0,0 +1,44 @@
+class Ability
+  include CanCan::Ability
+
+  attr_reader :user
+
+  def initialize(user)
+    return guest_abilities unless user.is_a?(User)
+    return admin_abilities if user.admin?
+
+    user_abilities(user)
+  end
+
+  private
+
+  def guest_abilities
+    can :read, :all
+    can :create, Authorization
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def user_abilities(user)
+    can :read, :all
+
+    can :create, [Question, Answer, Comment]
+    can %i[update destroy], [Question, Answer], { user_id: user.id }
+
+    alias_action :vote, :revote, to: :vote_actions
+
+    can :vote_actions, [Answer, Question] do |votable|
+      votable.user_id != user.id
+    end
+
+    can :mark_as_best, Answer, question: { user_id: user.id }
+
+    can :destroy, ActiveStorage::Attachment do |file|
+      file.record.user_id == user.id
+    end
+
+    can :destroy, Link, linkable: { user_id: user.id }
+  end
+end
