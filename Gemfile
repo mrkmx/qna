a886201e0400adbdbabc1fbba4d90414318cb4d7
mrkmx <alex.a.maximow@gmail.com>
2021-09-28 14:21:09 +0400

    add capistrano

--- Gemfile
+++ Gemfile
@@ -64,6 +64,13 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'seed_dump'
+
+  gem 'capistrano', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-passenger', require: false
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-rvm', require: false
+  gem 'capistrano-sidekiq', require: false
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "qna"
+set :repo_url, "git@github.com:mrkmx/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, "deployer"
+set :pty, false
+set :init_system, :systemd
+set :service_unit_name, "sidekiq"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
